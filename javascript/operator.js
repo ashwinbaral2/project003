1e726157ead33d2a6eaee52eeeebe0ddea131d4f
Ashwin Baral <140883065+ashwinbaral2@users.noreply.github.com>
2024-04-23 08:39:29 +0545

    added two numbers and a boolean operation

---
+++ javascript/operator.js
@@ -0,0 +1,13 @@
+//Q1 create two constants, add them assign to a new constants
+const x = 8;
+const y = 16;
+let z = x + y;
+console.log(z);
+//Ans:24
+
+
+//Check below code, output should be false by comparing. console.log the answer
+const a = 10
+const b = '10'
+console.log(a===b);
+//false
