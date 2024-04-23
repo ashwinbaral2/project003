e9e6eea1031f7a6d0b25148d4bb3efa7a72ca2aa
Ashwin Baral <140883065+ashwinbaral2@users.noreply.github.com>
2024-04-23 09:14:44 +0545

    array operation

---
+++ javascript/array/array.js
@@ -0,0 +1,17 @@
+const arr = [3,5,2]
+//Q1 calculate length of array and console log
+//expected output is 3
+console.log(arr.length); //3
+
+
+//Q2 reverse the array and console log
+//expected output [2,5,3]
+
+let reversedArr= arr.reverse();
+console.log(reversedArr); //[2, 5, 3]
+
+
+///Q3 join the array and console log
+// expected output 352
+let joinedStr = arr.join("");
+console.log(joinedStr); //['253']
