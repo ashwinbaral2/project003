6f3364c0afc73fe0a60efd74a13e7062e9a573b6
Ashwin Baral <140883065+ashwinbaral2@users.noreply.github.com>
2024-04-23 09:00:38 +0545

    random number generated and checked if greater than 3

--- javascript/operator.js
+++ javascript/operator.js
@@ -11,3 +11,15 @@ const a = 10
 const b = '10'
 console.log(a===b);
 //false
+
+// generate a random number between 0 and 5, if it is lesser than 3,
+//console.log "lesser than 3" else "Greater"
+
+const randomNumber = Math.floor(Math.random() * 6); // Random number between 0 (inclusive) and 6 (exclusive)
+
+// Check if the random number is less than 3
+if (randomNumber < 3) {
+    console.log("lesser than 3");
+} else {
+    console.log("Greater");
+}
